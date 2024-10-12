9afb4ca909041fd284a4603da82231f707b27985
Adrian Vilela <vilelaa521@gmail.com>
2024-10-11 23:01:01 -0500

    Guarde el codigo en formato .py

---
+++ python/ruleta.py
@@ -0,0 +1,12 @@
+import random
+import os
+import time
+
+print("\t-        RULETA        -")
+print("\tOpcion 1: 1-18/19-36 ......... x2")
+print("\tOpcion 2: 1-12/13-24/25-36 ... x3")
+print("\tOpcion 3: Rojo/Negro ......... x2")
+print("\tOpcion 4: Par/Impar .......... x2")
+print("\tOpcion 5: Cualquier numero ... x31")
+
+num_ruleta = int(input("Ingrese el numero a apostar: "))
