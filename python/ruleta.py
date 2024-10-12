3e5bc26bf99f1b45549809b63931da4e0d8b6f12
Adrian Vilela <vilelaa521@gmail.com>
2024-10-12 16:05:19 -0500

    Termine el tablero de la ruleta, falta agregar la seleccion del numero para la ruleta

--- python/ruleta.py
+++ python/ruleta.py
@@ -33,7 +33,7 @@ def menu_opcion(opcion):
             opcion_1 = int(input("Ingrese la opcion a elegir "))
 
             while i < 0:
-                if opcion_1 > 3 or opcion_1 < 0:
+                if opcion_1 > 3 or opcion_1 < 1:
                     opcion_1 = int(input("Opcion no valida, intentelo nuevamente: "))
                 else:
                     i += 1
@@ -82,7 +82,7 @@ def menu_opcion(opcion):
             opcion_1 = int(input("Ingrese la opcion a elegir "))
 
             while i < 0:
-                if opcion_1 > 4 or opcion_1 < 0:
+                if opcion_1 > 4 or opcion_1 < 1:
                     opcion_1 = int(input("Opcion no valida, intentelo nuevamente: "))
                 else:
                     i += 1
@@ -142,7 +142,7 @@ def menu_opcion(opcion):
             opcion_1 = int(input("Ingrese la opcion a elegir "))
 
             while i < 0:
-                if opcion_1 > 4 or opcion_1 < 0:
+                if opcion_1 > 4 or opcion_1 < 1:
                     opcion_1 = int(input("Opcion no valida, intentelo nuevamente: "))
                 else:
                     i += 1
@@ -191,7 +191,7 @@ def menu_opcion(opcion):
             opcion_1 = int(input("Ingrese la opcion a elegir "))
 
             while i < 0:
-                if opcion_1 > 4 or opcion_1 < 0:
+                if opcion_1 > 4 or opcion_1 < 1:
                     opcion_1 = int(input("Opcion no valida, intentelo nuevamente: "))
                 else:
                     i += 1
@@ -232,8 +232,35 @@ def menu_opcion(opcion):
 
         case 5:
             
-            print("\t | |")
-            print("\t\033[37m", " |", "\033[32m", "0", "\033[37m", "|", sep="")
-            print("\t\033[37m", "| |")
+            print("\t |   |", "\033[31m", " 1 ", "\033[37m", "| 4 |", "\033[31m", " 7 ", "\033[37m", "| 10 | 13 |", "\033[31m", " 16 ", "\033[37m", "|", 
+            "\033[31m", " 19 ", "\033[37m", "| 22 |", "\033[31m", " 25 ", "\033[37m", "| 28 | 31 |", "\033[31m", " 34 ", "\033[37m", "|", sep="")
+            print("\t\033[37m", " |", "\033[32m", " 0 ", "\033[37m", "| 2 |", "\033[31m", " 5 ", "\033[37m", "| 8 | 11 |", "\033[31m", " 14 ", "\033[37m", 
+            "| 17 | 20 |", "\033[31m", " 23 ", "\033[37m", "| 26 | 29 |", "\033[31m", " 32 ", "\033[37m", "| 35 |", sep="")
+            print("\t\033[37m", " |   |", "\033[31m", " 3 ", "\033[37m", "| 6 |", "\033[31m", " 9 ", "\033[37m", "|", "\033[31m", " 12 ", "\033[37m", "| 15 |", 
+            "\033[31m", " 18 ", "\033[37m", "|", "\033[31m", " 21 ", "\033[37m", "| 24 |", "\033[31m", " 27 ", "\033[37m", "|", "\033[31m", " 30 ", "\033[37m", "| 33 |", "\033[31m", " 36 ", "\033[37m", "|", sep="")
+
+            num_jugador = int(input("Ingrese el numero a apostar"))
+
+            while i < 0:
+                if num_jugador > 36 or num_jugador < 0:
+                    num_jugador = int(input("Numero ingresado no valido, intentelo nuevamente: "))
+                else:
+                    i += 1
+
+            i = 0
+
+            apuesta = float(input("Ingrese la cantidad a apostar: "))
+
+            while i < 0:
+                if apuesta > saldo or apuesta < 0:
+                    apuesta = float(input("La cantidad ingresada no es valida, intentelo nuevamente: "))
+                else:
+                    i += 1
+
+            i = 0
+
+        case 6:
+
+            exit()
         
 menu_principal()
